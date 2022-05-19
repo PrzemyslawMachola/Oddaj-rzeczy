d5be818300d79e53cf8c67b6d0f8a3e9ec1e725d
PrzemyslawMachola <przemyslaw.machola@gmail.com>
2022-05-19 10:07:36 +0200

    base structure

---
+++ oddaj/src/App.js
@@ -0,0 +1,16 @@
+import './scss/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          oddaj coś
+        </p>
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
