e1eaefeb2aa80941481a556c6037e20b1dfd54fb
PrzemyslawMachola <przemyslaw.machola@gmail.com>
2022-05-24 21:51:05 +0200

    hero style

--- oddaj/src/components/hero.js
+++ oddaj/src/components/hero.js
@@ -1,7 +1,8 @@
 import React from "react";
 import UserNav from "./userNav";
 import MainNav from "./mainNav";
-import heroImage from "../assets/Home-Hero-Image.jpg"
+import heroImage from "../assets/Home-Hero-Image.jpg";
+import decoration from "../assets/Decoration.svg";
 
 const Hero = () => {
     return (
@@ -10,7 +11,15 @@ const Hero = () => {
             <div className="hero__content">
                 <UserNav />
                 <MainNav />
-                zacznij pomagać
+                <div className="hero__box">
+                    <span>Zacznij pomagać!</span>
+                    <span>Oddaj niechciane rzeczy w zaufane ręce</span>
+                    <img className="decoration" alt="decoration" src={decoration}></img>
+                    <div className="hero__buttons">
+                        <button className="hero__button">ODDAJ<br></br>RZECZY</button>
+                        <button className="hero__button">ZORGANIZUJ<br></br>ZBIÓRKĘ</button>
+                    </div>
+                </div>
             </div>
         </div>
     )
--- oddaj/src/components/mainNav.js
+++ oddaj/src/components/mainNav.js
@@ -1,8 +1,17 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import "../scss/elements/mainNav.scss";
+
 
 const MainNav = () => {
     return (
-        <div>start, o co chodzi</div>
+        <div className="main__nav">
+            <Link className="mainNavButton" to="/">Start</Link>
+            <Link className="mainNavButton" to="/">O co chodzi?</Link>
+            <Link className="mainNavButton" to="/">O nas</Link>
+            <Link className="mainNavButton" to="/">Fundacja i organizacje</Link>
+            <Link className="mainNavButton" to="/">Kontakt</Link>
+        </div>
     )
 }
 
